8c215ae9bb2dc449632e28836d94837d38c94000
dmitryreaper <dimadimof81@gmail.com>
2024-07-27 14:18:32 +0300

    update

--- startdwm.sh
+++ startdwm.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-setxkbmap -layout us,ru && setxkbmap -option 'grp:alt_shift_toggle' &
+#nohup setxkbmap -layout us,ru && setxkbmap -option 'grp:alt_shift_toggle' &
 nohup xsetroot -cursor_name left_ptr &
 nohup setxkbmap -option "ctrl:swapcaps" & 
-nohup feh --bg-scale ~/Pictures/2.png &
-nohup dwmblocks &
+nohup feh --bg-scale ~/Pictures/3.jpg &
+dwmblocks &
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 0
+xinput --set-prop 11 'libinput Accel Speed' 0.4
 nohup picom &
+xrandr --output eDP-1 --off
+xrandr --output HDMI-1 --auto
 
 while true; do
 	dwm 2> ~/.dwm.log
